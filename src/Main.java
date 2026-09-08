cd086b4f0c61cd9d0bb66cffb99e118fa8c14863
Christian Rupp <nosferatu_rupp@hotmail.com>
2026-09-08 12:18:12 +0200

    Konstruktor für Tiere verwendet

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,17 @@ public class Main {
         Tier tier1 = new Tier("Simba", "Löwe", 5, "ROOAAARRR!!");
         Tier tier2 = new Tier("Dumbo", "Elefant", 12, "TÖRÖÖÖÖ!!");
         Tier tier3 = new Tier("Charly", "Affe", 4 , "UH UH AH AH !!");
+        Tier tier4 = new Tier("Balu", "Bär", 7, "BRUMM!!");
 
         tier1.anzeigen();
         tier2.anzeigen();
         tier3.anzeigen();
+        tier4.anzeigen();
 
         tier1.geraeuschMachen();
         tier2.geraeuschMachen();
         tier3.geraeuschMachen();
-
+        tier4.geraeuschMachen();
 
 
 
