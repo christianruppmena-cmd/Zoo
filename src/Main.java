cbbda15cf6e7a9c1b649d3a48725a5e968a543dd
Christian Rupp <nosferatu_rupp@hotmail.com>
2026-09-08 12:14:48 +0200

    Konstruktor für Tiere hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
-        Tier tier1 = new Tier();
-        Tier tier2 = new Tier();
-        Tier tier3 = new Tier();
-
-        tier1.name = "Simba";
-        tier1.art = "Löwe";
-        tier1.alter = 5;
-        tier1.geraeusch = "ROAARRR!";
-
-        tier2.name = "Dumbo";
-        tier2.art = "Elefant";
-        tier2.alter = 12;
-        tier2.geraeusch = "TÖRRRÖÖÖÖ!";
-
-        tier3.name = "Charly";
-        tier3.art = "Affe";
-        tier3.alter = 4;
-        tier3.geraeusch = "UH UH AH AH!";
+        Tier tier1 = new Tier("Simba", "Löwe", 5, "ROOAAARRR!!");
+        Tier tier2 = new Tier("Dumbo", "Elefant", 12, "TÖRÖÖÖÖ!!");
+        Tier tier3 = new Tier("Charly", "Affe", 4 , "UH UH AH AH !!");
 
         tier1.anzeigen();
         tier2.anzeigen();
--- src/Tier.java
+++ src/Tier.java
@@ -4,6 +4,13 @@ public class Tier {
     int alter;
     String geraeusch;
 
+    Tier(String name, String art, int alter, String geraeusch){
+        this.name = name;
+        this.art = art;
+        this.alter = alter;
+        this.geraeusch = geraeusch;
+    }
+
     void anzeigen(){
 
         System.out.println("Name: " + name);
