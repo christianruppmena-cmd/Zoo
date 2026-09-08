47e3654d72142418c1e2ed694587c3b416293815
Christian Rupp <nosferatu_rupp@hotmail.com>
2026-09-08 12:08:28 +0200

    Geräusche für Tiere hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,25 @@ public class Main {
         tier1.name = "Simba";
         tier1.art = "Löwe";
         tier1.alter = 5;
+        tier1.geraeusch = "ROAARRR!";
 
         tier2.name = "Dumbo";
         tier2.art = "Elefant";
         tier2.alter = 12;
+        tier2.geraeusch = "TÖRRRÖÖÖÖ!";
 
         tier3.name = "Charly";
         tier3.art = "Affe";
         tier3.alter = 4;
+        tier3.geraeusch = "UH UH AH AH!";
 
         tier1.anzeigen();
         tier2.anzeigen();
         tier3.anzeigen();
+
+        tier1.geraeuschMachen();
+        tier2.geraeuschMachen();
+        tier3.geraeuschMachen();
 
 
 
--- src/Tier.java
+++ src/Tier.java
@@ -2,11 +2,15 @@ public class Tier {
     String name;
     String art;
     int alter;
+    String geraeusch;
 
     void anzeigen(){
 
         System.out.println("Name: " + name);
         System.out.println("Art: " + art);
         System.out.println("Alter: " + alter);
+    }
+    void geraeuschMachen(){
+        System.out.println(name + " macht: " + geraeusch);
     }
 }
