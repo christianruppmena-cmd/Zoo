349f9f57f8e1a95b8b0a34579e287cabb69e83e6
Christian Rupp <nosferatu_rupp@hotmail.com>
2026-09-10 08:42:51 +0200

    Zoo-Methoden öffentlich zugänglich gemacht

--- src/Zoo.java
+++ src/Zoo.java
@@ -4,23 +4,23 @@ import java.util.ArrayList;
 public class Zoo {
     private ArrayList<Gehege> gehege = new ArrayList<>();
 
-    void gehegeHinzufuegen(Gehege gehege){
+    public void gehegeHinzufuegen(Gehege gehege){
         this.gehege.add(gehege);
     }
 
-    void alleTiereAnzeigen(){
+    public void alleTiereAnzeigen(){
         for (Gehege gehege : this.gehege){
             gehege.alleTiereAnzeigen();
         }
     }
 
-    void alleGeraeuscheMachen(){
+    public void alleGeraeuscheMachen(){
         for (Gehege gehege : this.gehege){
             gehege.alleGeraeuschMachen();
         }
     }
 
-    void alleGehegeAnzeigen(){
+    public void alleGehegeAnzeigen(){
         for (Gehege gehege : this.gehege){
             gehege.kurzAnzeigen();
         }
@@ -67,7 +67,7 @@ public class Zoo {
     }
 
 
-    void alleTiereFuettern(){
+    public void alleTiereFuettern(){
         for (Gehege gehege : this.gehege){
             gehege.alleTiereFuettern();
         }
